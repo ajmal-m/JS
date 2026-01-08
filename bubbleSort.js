8bdf2fb061b2311aa4cabc20740d4daef44fe092
ajmal-m <ajmalm76774@gmail.com>
2026-01-08 10:46:59 +0530

    Bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,12 @@
+// Time Complexity O(n^2)T
+const BubbleSort = (arr) => {
+   let len = arr.length;
+   for(let i=0; i< len; i++){
+       for(let j=0; j< (len-i-1); j++){
+           if( arr[j] > arr[j+1]) {
+               [ arr[j] , arr[j+1]] = [ arr[j+1], arr[j]];
+           }
+       }
+   }
+   return arr
+};

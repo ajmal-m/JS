14e10c52d8ccd9da3b064a20d7d8992a3e5b0829
ajmal-m <ajmalm76774@gmail.com>
2026-01-08 10:04:35 +0530

    Binary search

---
+++ binarysearch.js
@@ -0,0 +1,16 @@
+const BinarySearch = (arr, target) => {
+    let start = 0;
+    let end = arr.length-1;
+    while(start < end){
+        const mid = Math.floor((start+end)/2);
+        if( arr[mid] === target ){
+            return mid;
+        }
+        if( arr[mid] > target ){
+            end = mid-1;
+        }else{
+            start = mid+1;
+        }
+    }
+    return -1;
+};

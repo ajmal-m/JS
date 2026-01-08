2e1aa7f232a68c4449f4a6d5682df937df57db5c
ajmal-m <ajmalm76774@gmail.com>
2026-01-08 10:55:57 +0530

    Selection sort

---
+++ selectionSort.js
@@ -0,0 +1,14 @@
+// Time Complexity O(n^2)T
+const SelectionSOrt = (arr) => {
+    const n = arr.length;
+    for(let i=0;i<n;i++){
+        let minIndex = i;
+        for(let j=i+1; j<n ; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        [ arr[i] , arr[minIndex] ]=[arr[minIndex] , arr[i] ]
+    }
+    return arr;
+};
